46246d8ce3d0860bc81a36e58734e7b20e07293b
opengl-8080 <tomcat.port.8080+github@gmail.com>
2018-03-19 23:33:35 +0900

    タッチ実装リファクタリング

--- src/main/java/jimgv/controller/single/SingleImageController.java
+++ src/main/java/jimgv/controller/single/SingleImageController.java
@@ -11,7 +11,10 @@ import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.*;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TouchEvent;
+import javafx.scene.input.ZoomEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -54,20 +57,70 @@ public class SingleImageController implements Initializable {
     private ImageView imageView;
     private SingleImage singleImage;
     private ContextMenu contextMenu = new ContextMenu();
-    private MouseGesture mouseGesture = new MouseGesture();
+    private TouchGesture touchGesture = new TouchGesture();
     private OpenedDirectory openedDirectory;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         singleImage = new SingleImage(imageView);
         
-        mouseGesture.setLeftDragListener(singleImage::stopTranslate);
+        touchGesture.setDoubleTapListener((x, y) -> {
+            singleImage.resetZoom();
+        });
+
+        touchGesture.setTouchPressedListener((x, y) -> {
+            singleImage.startTranslate();
+        });
         
-        mouseGesture.setRightScrollListener(singleImage::startZoom);
+        touchGesture.setTouchMoveListener((dx, dy) -> {
+            singleImage.stopTranslate(dx, dy);
+
+            if (!singleImage.isZoomed()) {
+                double rate = Math.abs(dx) / stage.getWidth();
+
+                double Ys = 0.5;
+                double Ye = 0.2;
+                double Xs = 0.15;
+                double Xe = 0.4;
+                double DX = Xe-Xs;
+                double DY = Ys-Ye;
+                if (rate < Xs) {
+                    imageView.setOpacity(1.0);
+                } else if (Xs <= rate && rate < Xe) {
+                    imageView.setOpacity(DY*(Xe-rate)/DX + Ye);
+                } else {
+                    imageView.setOpacity(Ye);
+                }
+            }
+        });
+
+        touchGesture.setTouchReleasedListener((dx, dy) -> {
+            if (!singleImage.isZoomed()) {
+                singleImage.resetZoom();
+                double rate = Math.abs(dx) / stage.getWidth();
+
+                double E = 0.15;
+
+                if (E < rate) {
+                    if (dx < 0) {
+                        Path previous = openedDirectory.previous();
+                        singleImage.load(previous);
+                    } else {
+                        Path next = openedDirectory.next();
+                        singleImage.load(next);
+                    }
+                }
+            }
+            
+            imageView.setOpacity(1.0);
+        });
         
-        mouseGesture.setScrollListener(delta -> {
-            Path imagePath = delta < 0 ? openedDirectory.previous() : openedDirectory.next();
-            singleImage.load(imagePath);
+        touchGesture.setZoomListener(rate -> {
+            singleImage.startZoom(rate);
+        });
+        
+        touchGesture.setZoomFinishedListener(() -> {
+            singleImage.stopZoom();
         });
     }
 
@@ -111,103 +164,31 @@ public class SingleImageController implements Initializable {
     @FXML
     public void onTouchPressed(TouchEvent e) {
         contextMenu.hide();
-
-        mouseGesture.onMousePressed(e);
-
-        if (e.getTouchCount() == 1) {
-            singleImage.startTranslate();
-            fromX = e.getTouchPoint().getScreenX();
-        }
+        touchGesture.onTouchPressed(e);
     }
 
     @FXML
     public void onTouchReleased(TouchEvent e) {
-        mouseGesture.onMouseReleased();
-
-        if (e.getTouchCount() == 1 && fromX != null) {
-            double dx = fromX - e.getTouchPoint().getScreenX();
-
-            if (!singleImage.isZoomed()) {
-                singleImage.resetZoom();
-                double rate = Math.abs(dx) / stage.getWidth();
-
-                double E = 0.15;
-
-                if (E < rate) {
-                    if (dx < 0) {
-                        Path previous = openedDirectory.previous();
-                        singleImage.load(previous);
-                    } else {
-                        Path next = openedDirectory.next();
-                        singleImage.load(next);
-                    }
-
-                    fromX = null;
-                }
-            }
-        }
-        fromX = null;
-        imageView.setOpacity(1.0);
+        touchGesture.onTouchReleased(e);
     }
-
-    private Double fromX;
     
     @FXML
     public void onTouchMoved(TouchEvent e) {
-        mouseGesture.onMouseDragged(e);
-
-        if (e.getTouchCount() == 1 && fromX != null) {
-            double dx = fromX - e.getTouchPoint().getScreenX();
-
-            if (!singleImage.isZoomed()) {
-                double rate = Math.abs(dx) / stage.getWidth();
-
-//                double S = 0.08;
-//                double E = 0.3;
-//                if (S < rate) {
-//                    imageView.setOpacity((E - rate) / (E - S));
-//                }
-                double Ys = 0.5;
-                double Ye = 0.2;
-                double Xs = 0.15;
-                double Xe = 0.4;
-                double DX = Xe-Xs;
-                double DY = Ys-Ye;
-                if (rate < Xs) {
-                    imageView.setOpacity(1.0);
-                } else if (Xs <= rate && rate < Xe) {
-                    imageView.setOpacity(DY*(Xe-rate)/DX + Ye);
-                } else {
-                    imageView.setOpacity(Ye);
-                }
-            }
-        }
+        touchGesture.onTouchMoved(e);
     }
 
     @FXML
     public void onMouseClicked(MouseEvent e) {
-        if (e.getClickCount() == 2) {
-            singleImage.resetZoom();
-        }
-    }
-    
-//    @FXML
-    public void onScroll(ScrollEvent e) {
-        mouseGesture.onScroll(e);
+        touchGesture.onMouseClicked(e);
     }
     
     @FXML
     public void onZoom(ZoomEvent e) {
-        singleImage.startZoom(e.getZoomFactor());
+        touchGesture.onZoom(e);
     }
     
     @FXML
     public void onZoomFinished() {
-        singleImage.stopZoom();
-    }
-
-    @FXML
-    public void onSwipeLeft(SwipeEvent e) {
-        System.out.println("swipe left");
+        touchGesture.onZoomFinished();
     }
 }
